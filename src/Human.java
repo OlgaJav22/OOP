73c53edd7c29ddc952a004ba10abb1bd31ca8aad
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-11-14 17:03:18 +0300

    задание 1 выполнено

--- src/Human.java
+++ src/Human.java
@@ -1,12 +1,29 @@
 public class Human {
-    String name;
-    int yearOfBirth;
-    String town;
-    public  Numan (String name, int yearOfBirth, String town) {
+    private String name;
+    private int yearOfBirth;
+    private String town;
+
+    public  Human (String name, int yearOfBirth, String town) {
         this.name = name;
         this.yearOfBirth = yearOfBirth;
         this.town = town;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public int getYearOfBirth() {
+        return this.yearOfBirth;
+    }
+
+    public String getTown() {
+        return this.town;
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + getName() + ". Я из города " + getTown() + ". Я родился " + getYearOfBirth() + " в году. Будем знакомы!";
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Задание №1
+
+        Human maksim = new Human("Максим", 1988, "Минск");
+        Human anna = new Human("Аня", 1993, "Москва");
+        Human katya = new Human("Катя", 1992, "Калининград");
+        Human artem = new Human("Артем", 1995, "Москв");
+
+        System.out.println(maksim);
+        System.out.println(anna);
+        System.out.println(katya);
+        System.out.println(artem);
+
+
     }
 }
