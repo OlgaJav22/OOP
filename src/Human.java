b6aa46e75bac676819435beffe6b0e62e72af5cd
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-11-14 16:28:19 +0300

    Начало д/з

--- src/Human.java
+++ src/Human.java
@@ -2,6 +2,11 @@ public class Human {
     String name;
     int yearOfBirth;
     String town;
+    public  Numan (String name, int yearOfBirth, String town) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.town = town;
+    }
 
 
 }
